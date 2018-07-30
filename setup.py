5caf93c3e59b6a6eedb6aa134f6da494a86a357f
Gustavo Fonseca <gustavo@gfonseca.net>
2018-07-30 15:14:57 -0300

    Atualiza o setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup, find_packages
+from setuptools import setup
+
 
 requires = [
     "legendarium>=1.2.0"
@@ -10,13 +8,15 @@ requires = [
 
 setup(
     name="xylose",
-    version='1.33.1',
-    description="A SciELO library to abstract a JSON data structure that is a product of the ISIS2JSON conversion using the ISIS2JSON type 3 data model.",
+    version='1.33.2',
+    description="A SciELO library to abstract a JSON data structure that is a "
+                "product of the ISIS2JSON conversion using the ISIS2JSON type "
+                "3 data model.",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
     license="BSD 2-clause",
     url="http://docs.scielo.org",
-    packages=find_packages(),
+    packages=['xylose'],
     include_package_data=True,
     classifiers=[
         "Development Status :: 1 - Planning",
