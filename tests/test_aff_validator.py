1bf081384ea3ffc7577ee1b129e5df690b3f0b29
Roberta Takenaka <roberta.takenaka@scielo.org>
2020-02-07 15:42:17 -0300

    Cria função para analisar se existe conflitos

--- tests/test_aff_validator.py
+++ tests/test_aff_validator.py
@@ -2,13 +2,34 @@ import unittest
 
 from xylose.aff_validator import (
     is_a_match,
+    has_conflicts,
 )
 
 
 class TestIsAMatch(unittest.TestCase):
-
     def test_return_true_if_strings_are_equal(self):
         self.assertTrue(is_a_match("a", "a"))
 
     def test_return_false_if_strings_are_different(self):
         self.assertFalse(is_a_match("MG", "SP"))
+
+
+class TestHasConflicts(unittest.TestCase):
+    def test_return_conflicts_if_aff_data_do_not_match(self):
+        norm_aff = {
+            "state": "DF",
+            "city": "",
+            "country_iso_3166": "BR",
+            "institution": "Universidade",
+            "index": "A01",
+        }
+        original_aff = {
+            "state": "SP",
+            "city": "São Paulo",
+            "country_iso_3166": "BR",
+            "institution": "Universidade de São Paulo",
+            "index": "A01",
+        }
+        expected = [("state", "SP", "DF")]
+        result = has_conflicts(original_aff, norm_aff)
+        self.assertEqual(expected, result)
--- xylose/aff_validator.py
+++ xylose/aff_validator.py
@@ -1,5 +1,3 @@
-
-
 def normalize_value(s):
     return s.upper()
 
@@ -8,3 +6,21 @@ def is_a_match(original, normalized):
     if normalize_value(original) == normalize_value(normalized):
         return True
     return False
+
+
+def has_conflicts(original_aff, normaff):
+    if original_aff:
+        conflicts = []
+        for label in ["country_iso_3166", "state", "city"]:
+
+            original = original_aff.get(label)
+            normalized = normaff.get(label)
+
+            if original and normalized:
+
+                if is_a_match(original, normalized):
+                    continue
+
+                conflicts.append((label, original, normalized))
+
+        return conflicts
